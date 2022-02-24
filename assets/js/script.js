f0f88677199d9a4b5aa3b0367ae43d6ad1b42521
Stian Lystad <Stianlystad_1@hotmail.com>
2022-02-24 22:05:14 +0000

    add script structure to test

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,34 +3,92 @@
  * and choices
  */
 
-const buttons = document.getElementById("control");
+const buttons = document.getElementById("controls");
 const playerScore = document.getElementById("results-1");
 const computerScore = document.getElementById("results-2");
 const messages = document.getElementById("messages");
 const choices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]
+const controls = document.querySelectorAll(".control");
 
 /**
  * Add event listener to buttons
  * 
  */
 
-for (let button for buttons) {
-    button.addEventListener("click", function() {
-        let playerChoice = this.getAttribute("data-choice");
-        playGame(playerChoice);
+controls.forEach((control) => {
+    control.addEventListener("click", function(){
+        const pInput = this.textContent;
+
+        const cControls =["Rock", "Paper", "Scissors", "Lizard", "Spock"];
+        const cInput = cControls[Math.floor(Math.random()* 5)];
     });
-}
+});
 
 /**
- * Main game function
+ * compare inputs
  */
-function playGame(playerChoice) {
-let computerChoice = Math.floor(Math.random()* 3);
+function compareInputs(pInput, cInput) {
+    const currentMatch = `${pInput} vs ${cInput}`;
 
-let result = checkWinner(choices[computerChoice], choices[playerChoice]);
+    // Tie check
+    if (pInput === cInput) {
+        alert(`${currentMatch} is a Tie`);
+        return;
+    }
+}
 
-updateScores(result);
+// Rock
+if (pInput === "Rock") {
+    if (cInput === "Scissors") {
+        alert(`${currentMatch} = You Win`);
+    } else if (cInput === "Lizard") {
+        alert(`${currentMatch} = You Win`)
+    } else {
+        alert(`${currentMatch} = Computer Wins`)
+    }
+}
+// Paper
+else if (pInput === "Paper") {
+    if (cInput === "Rock") {
+        alert(`${currentMatch} = You Win`);
+    } else if (cInput === "Spock")  {
+        alert(`${currentMatch} = You Win`);
+    } else {
+        alert(`${currentMatch} = Computer Wins`);
+    }
+}
 
+// Scissors
+else if (pInput === "Scissors") {
+    if (cInput === "Paper") {
+        alert(`${currentMatch} = You Win`);
+    } else if (cInput === "Lizard") {
+        alert(`${currentMatch} = You Win`);
+    } else {
+        alert(`${currentMatch} = Computer Wins`);
+    }
+}
+
+// Lizard
+else if (pInput === "Lizard") {
+    if (cInput === "Spock") {
+        alert(`${currentMatch} = You Win`);
+    } else if (cInput === "Paper") {
+        alert(`${currentMatch} = You Win`);
+    } else {
+        alert(`${currentMatch} = Computer Wins`);
+    }
+}
+
+// Spock
+else if (pInput === "Spock") {
+    if (cInput === "Scissors") {
+        alert(`${currentMatch} = You Win`);
+    } else if (cInput === "Rock") {
+        alert(`${currentMatch} = You Win`);
+    } else {
+        alert(`${currentMatch} = Computer wins`);
+    }
 }
 
 
